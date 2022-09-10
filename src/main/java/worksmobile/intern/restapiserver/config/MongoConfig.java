c478920f85b7dfe707fe0b5d86c4336a4c308fdf
blackSooooo <71834320+blackSooooo@users.noreply.github.com>
2022-09-10 22:04:37 +0900

    feat: Add mongo, redis, mapper config

---
+++ src/main/java/worksmobile/intern/restapiserver/config/MongoConfig.java
@@ -0,0 +1,19 @@
+package worksmobile.intern.restapiserver.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+
+import javax.annotation.PostConstruct;
+
+@Configuration
+public class MongoConfig {
+    @Autowired
+    private MappingMongoConverter mappingMongoConverter;
+
+    @PostConstruct
+    public void setUpMongoEscapeCharacterConversion() {
+        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+    }
+}
